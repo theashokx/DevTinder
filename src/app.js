38e31b03605da373ad781e0d80291f9946b2ad13
theashokx <theashokx@gmail.com>
2025-10-11 13:10:38 +0530

    MiddleWares,APIs

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 const express = require("express");
+
+const connectDB = require("./config/database");
+const User = require("./models/User");
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.send("Hello test Server");
+app.post("/signup", async (req, res) => {
+  const userObject = {
+    firstName: "Raj",
+    lastName: "Kumar",
+    emailId: "raj@gamil.com",
+    password: "rajkumar@123",
+  };
+
+  const user = new User(userObject);
+
+  try {
+    await user.save();
+    res.send("Sucessfully logged In");
+  } catch (err) {
+    res.status(400).send("Error saving in the user");
+  }
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello Hero");
-});
+connectDB()
+  .then(() => {
+    console.log("Connection successfully");
 
-app.listen("3000", () => {
-  console.log("Server is succesfully listening on port 3000");
-});
+    app.listen("3000", () => {
+      console.log("Server is succesfully listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("COnnection Failed");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://theashokx1:IEpzSxUNiLEiBw9T@cluster0.d50upzk.mongodb.net/devTinder"
+  );
+};
+
+module.exports = connectDB;
