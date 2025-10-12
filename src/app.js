47b0c72222753c897a7364bc16e0e00b8d946b9a
theashokx <theashokx@gmail.com>
2025-10-12 16:52:55 +0530

    validations

--- src/app.js
+++ src/app.js
@@ -3,22 +3,83 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/User");
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const userObject = {
-    firstName: "Raj",
-    lastName: "Kumar",
-    emailId: "raj@gamil.com",
-    password: "rajkumar@123",
-  };
+  // const userObject = {
+  //   firstName: "Raj",
+  //   lastName: "Kumar",
+  //   emailId: "raj@gamil.com",
+  //   password: "rajkumar@123",
+  // };
 
-  const user = new User(userObject);
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("Sucessfully logged In");
   } catch (err) {
-    res.status(400).send("Error saving in the user");
+    res.status(400).send("Error saving in the user" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  const user = await User.find({});
+  try {
+    console.log(user);
+    res.send(user);
+  } catch (err) {
+    console.log("Error");
+    res.status(400).send("Not Found");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const user = await User.find({ emailId: req.body.emailId });
+  try {
+    console.log(user);
+    res.send(user);
+  } catch (err) {
+    console.log("Error");
+    res.status(400).send("Not Found");
+  }
+});
+
+app.get("/delete", async (req, res) => {
+  const id = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    res.send(user + "User deleted successfully");
+  } catch (err) {
+    res.send("Sorry can't be deleted");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params();
+  const data = req.body;
+
+  try {
+    const allowedUpdates = [
+      "photoUrl",
+      "password",
+      "age",
+      "gender",
+      "about",
+      "skills",
+    ];
+    const isUpdateAllowed = Object.key
+      .key(data)
+      .every((k) => allowedUpdates.includes(k));
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    const user = await User.findOneAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    res.send("Succesfully updated");
+  } catch (err) {
+    res.send("Error");
   }
 });
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,25 +1,75 @@
 const mongoose = require("mongoose");
+var validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      // validate: [
+      //   {
+      //     validator: async function (email) {
+      //       const count = await mongoose.models.User.countDocuments({
+      //         emailId: email,
+      //       });
+      //       return count === 0;
+      //     },
+      //     message: "Email already exists",
+      //   },
+      //   {
+      //     validator: function (value) {
+      //       return validator.isEmail(value);
+      //     },
+      //     message: "Invalid emailId",
+      //   },
+      // ],
+      validate: {
+        validator: function (value) {
+          if (validator.isEmail(value)) throw new Error("Invalid Email Id");
+        },
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is Invalid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://w1.pngwing.com/pngs/743/500/png-transparent-circle-silhouette-logo-user-user-profile-green-facial-expression-nose-cartoon.png",
+    },
+    about: {
+      type: String,
+      default: "Description about the user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const userModel = mongoose.model("User", userSchema);
 
