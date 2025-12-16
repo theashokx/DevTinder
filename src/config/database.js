3d2a5db39ca29cd81ea7784ffbf0232b0136e0fb
theashokx <theashokx@gmail.com>
2025-12-16 23:24:43 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,1 @@
-const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://devTinder:6GToWXomWy71ll0c@devtinder.sptooju.mongodb.net/devTinder"
-  );
-};
-
-module.exports = connectDB;
