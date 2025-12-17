5ee003b7e4daa6dd3e9ce19cedc668bded3e933c
theashokx <theashokx@gmail.com>
2025-12-17 23:41:53 +0530

    Fix merge conflict in database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,8 @@
-
-<<<<<<< HEAD
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-process.env.MONGO_URI
-  );
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;
-=======
->>>>>>> 3d2a5db39ca29cd81ea7784ffbf0232b0136e0fb
