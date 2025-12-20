f340f3eb662c98b07d4708d70f5ffb3cb08e524f
theashokx <theashokx@gmail.com>
2025-12-20 22:37:00 +0530

    Done with feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const connectionRequestModel = require("../models/ConnectionRequest");
+const User = require("../models/User");
 
 const userRouter = express.Router();
 
@@ -50,6 +51,44 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.send({ message: "Sucessfully fetched the data", data: data });
   } catch (err) {
     res.send("Error : " + err.message);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user._id;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connections = await connectionRequestModel
+      .find({
+        $or: [{ fromUserId: loggedInUser }, { toUserId: loggedInUser }],
+      })
+      .select("fromUserId toUserId");
+
+    const hiddenUsersFromFeed = new Set();
+
+    connections.forEach((req) => {
+      hiddenUsersFromFeed.add(req.fromUserId.toString());
+      hiddenUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const feedUsers = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hiddenUsersFromFeed) } },
+        { _id: { $ne: loggedInUser } },
+      ],
+    })
+      .select(INFO_WANTED)
+      .skip(skip)
+      .limit(limit);
+
+    res.send({ message: "Feed fetched Successfully", data: feedUsers });
+  } catch (err) {
+    res.status(404).send({ message: err.message });
   }
 });
 
